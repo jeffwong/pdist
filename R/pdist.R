7ac1b37dcf498b73804d6683691148599b9538cc
jeffwong <jeff.ct.wong@gmail.com>
2012-11-20 00:22:45 -0800

    better data type handling, created s3 classes

--- R/pdist.R
+++ R/pdist.R
@@ -1,11 +1,23 @@
 pdist = function(X, Y = NULL, indices.A = NULL, indices.B = NULL) {
-  if (is.null(Y)) {
+  if (!is.null(Y)) {
+      if (X == Y) return (as.matrix(dist(X, upper=T)))
+  }
+  else {
+    if (is.null(indices.A) & is.null(indices.B))
+      return (as.matrix(dist(X, upper=T)))
+    if (length(indices.A) == nrow(X) & length(indices.B) == nrow(X))
+      return (as.matrix(dist(X, upper=T)))
+  }
+   
+  if (is.null(Y) & (!is.null(indices.A) & !is.null(indices.B))) {
       Y = X[indices.B,]
       X = X[indices.A,]
   }
-  
-  if(!is.matrix(X)) X = as.matrix(X)
-  if(!is.matrix(Y)) Y = as.matrix(Y)
+ 
+  if (is.vector(X)) X = matrix(X, nrow = 1)
+  if (is.vector(Y)) Y = matrix(Y, nrow = 1)
+  if (!is.matrix(X)) X = as.matrix(X)
+  if (!is.matrix(Y)) Y = as.matrix(Y)
 
   nx = as.integer(nrow(X))
   ny = as.integer(nrow(Y))
@@ -18,5 +30,13 @@ pdist = function(X, Y = NULL, indices.A = NULL, indices.B = NULL) {
 
   result = .C("Rpdist", X.vec, Y.vec, nx, ny, p, distances=distances,
               NAOK = T)
-  matrix(result$distances, nrow(X), nrow(Y), byrow=T)
+  structure(list(dist = result$distances, 
+                 n = nrow(X),
+                 p = nrow(Y)), class = "pdist")
+}
+
+setClass("pdist", representation = "list", S3methods = T)
+
+as.matrix.pdist = function(x) {
+  matrix(x$dist, x$n, x$p, byrow=T)
 }
